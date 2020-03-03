d6e4c28631e1eefcdb559f38bf1db087c1daa6fd
kat <kjacasio@hawaii.edu>
2020-03-03 10:59:14 -1000

    Hackwich 6 Mar 3

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    var myFriendsArray = ["Ati","Leah","Brenna","Summer"]
+    var myTravelArray = ["Cali","Texas","Las Vegas"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       // myFriendsArray.count
+        myTravelArray.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myTravelArray[indexPath.row]
+        cell?.textLabel?.text = text
+        return cell!
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
